a02ce29ce69e49259ccac78236e2235a5c0d4537
rpasetes <russap01@gmail.co>
2025-10-07 17:50:23 -0400

    bye bye render trees

--- imagine-sisyphus-happy/src/renderer.ts
+++ imagine-sisyphus-happy/src/renderer.ts
@@ -1,13 +1,9 @@
 import { Application, Assets, Sprite } from "pixi.js";
 import type { GameState } from "./coordinator";
-import { Tree } from "./stateMachine";
 import { initializeUIElements, renderUI } from "./ui";
 import { initDevtools } from "@pixi/devtools";
 
 let app: Application;
-let treeTexture: any
-
-const myTrees: Map<string, Sprite> = new Map();
 
 // Initialize the application
 export async function initialize(gameState) {
@@ -20,14 +16,11 @@ export async function initialize(gameState) {
   // Append the application canvas to the document body
   document.getElementById("pixi-container")!.appendChild(app.canvas);
 
-  treeTexture = await Assets.load("/assets/tree.png");
-
   initializeUIElements(app);
 }
 
 // TODO: split rendering into the scene itself and the UI
 // TODO: write the UI
 export async function render(gameState: GameState) {
-  renderTrees(gameState.trees);
   renderUI(gameState.score, gameState.streak);
 }
