6921759bf77bd8ecb1a8c1eded4aba1a2f9854ff
Matthew Huff <matthewhuff89@gmail.com>
2025-10-07 17:17:20 -0400

    increase score/elevation and streak upon spacebar press

--- imagine-sisyphus-happy/src/coordinator.ts
+++ imagine-sisyphus-happy/src/coordinator.ts
@@ -1,13 +1,13 @@
 import * as stateMachine from "./stateMachine";
 import * as renderer from "./renderer";
-import * as inputs from './input'
+import * as inputs from "./input";
 import type { GameState } from "./stateMachine";
 
 let gameState: GameState;
 
 export function initializeGameState(): void {
   gameState = stateMachine.initialGameState;
-  inputs.initialize()
+  inputs.initialize();
   // passing it off to the renderer
   renderer.initialize(gameState);
 }
@@ -21,7 +21,7 @@ export function startGameLoop() {
 
 // This will run 60x per second.
 export function gameLoop() {
-  const newInputs = inputs.updateInputs()
+  const newInputs = inputs.updateInputs();
   // logic
   const newState = stateMachine.updateGame(newInputs, gameState); // call all the things that change it
 
--- imagine-sisyphus-happy/src/input.ts
+++ imagine-sisyphus-happy/src/input.ts
@@ -1,30 +1,30 @@
-
-export type KeyState = { pressed: boolean, justPressed: boolean }
-const keys: Map<string, KeyState> = new Map()
+export type KeyState = { pressed: boolean; justPressed: boolean };
+const keys: Map<string, KeyState> = new Map();
 
 export function initialize() {
-    window.addEventListener('keydown', e => {
-        if (!keys.get(e.code)) {
-            keys.set(e.code, { pressed: true, justPressed: true });
-        }
-    });
+  window.addEventListener("keydown", (e) => {
+    if (!keys.get(e.code)) {
+      keys.set(e.code, { pressed: true, justPressed: true });
+      console.log(keys.get(e.code))
+    }
+  });
 
-    window.addEventListener('keyup', e => {
-        keys.delete(e.code)
-    });
+  window.addEventListener("keyup", (e) => {
+    keys.delete(e.code);
+  });
 }
 
 // TODO: justPressed doesn't work
 // TODO: turn this into a nice input system that detects mouse movement too
 export function updateInputs(): Map<string, KeyState> {
-    function update() {
-        keys.forEach((v, k) => {
-            v.justPressed = false
-        })
-        requestAnimationFrame(update);
-    }
+  function update() {
+    keys.forEach((v, k) => {
+      v.justPressed = false;
+    });
+    requestAnimationFrame(update);
+  }
 
-    update();
+  update();
 
-    return keys
+  return keys;
 }
