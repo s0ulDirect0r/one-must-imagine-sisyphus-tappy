395eadbf0dc92eb50841b43b36a09ce39df8f7ec
Noa <noasouccar@icloud.com>
2025-10-08 17:49:06 -0400

    added new sissy character and found where collision points are

--- imagine-sisyphus-happy/src/Player.ts
+++ imagine-sisyphus-happy/src/Player.ts
@@ -1,63 +1,68 @@
-import { Application, Sprite, Texture, Rectangle, AnimatedSprite } from "pixi.js";
+import { Application, Sprite, Texture, Rectangle, AnimatedSprite, Graphics } from "pixi.js";
 import * as PIXI from "pixi.js";
 
+
+
+export type Player = {
+    x: number,
+    y: number,
+    sprite: AnimatedSprite
+    speed: 0.1
+}
+
+let player: Player;
 export async function initializePlayer(app: Application) {
-    const texture = await PIXI.Assets.load("/assets/Sisyphus.png"); // load asset
-    const baseTexture = texture.baseTexture;
+    const texture = await PIXI.Assets.load("/assets/sissypose1.png"); // load asset
+    const texture2 = await PIXI.Assets.load("/assets/sissypose2.png"); // load asset
 
 
-    const framesPerRow = 6;
-    const framesPerCol = 6;
-
-
-
-    const frames: Texture[] = [];
-
-    for (let i = 0; i < framesPerCol; i++) {
-        for (let j = 0; j < framesPerRow; j++) {
-
-            const frameRect = new Rectangle((2298 * j) / framesPerRow, (2136 * i) / framesPerCol, 383, 356);
-
-            const frame = new Texture({
-                source: baseTexture,
-                frame: frameRect
-            });
-
-            frame.updateUvs();
-
-            const sprite = new Sprite(frame);
-            // app.stage.addChild(sprite)
-
-
-            // Ensure it’s definitely visible
-            sprite.anchor.set(0); // no offset
-            sprite.x = 0;
-            sprite.y = 0;
-
-            // Optional: make it huge to be sure you see it
-            sprite.scale.set(0.1); // adjust if needed
-            frames.push(frame);
-        }
-
-    }
-
-    // ---- Static sprite ----
 
 
 
     // ---- Animation ----
-    const anim = new AnimatedSprite(frames);
-    anim.anchor.set(0.5);
-    anim.x = app.screen.width / 2;
-    anim.y = app.screen.height / 2 - 200;
-    anim.animationSpeed = 0.2;
-    app.stage.addChild(anim)
-    anim.play();
+    const anime = new AnimatedSprite([texture2]);
+    anime.anchor.set(0.5);
 
+    player = { sprite: anime, x: app.screen.width / 2, y: app.screen.height / 2 + 300, speed: 0.1 }
+    anime.x = player.x;
+    anime.y = player.y;
+    anime.animationSpeed = player.speed;
+    app.stage.addChild(anime)
+    anime.play();
 
+    const circle = new Graphics();
 
+    // Draw a red circle with a center at (100, 100) and a radius of 50
+    circle.ellipse(player.x + 5, player.y - 15, 75, 60) // points = (x - h)^2/75^2 + (y-k) ^2/60^2 at (h, k)
 
+    // Set the fill color to red (0xff0000)
+    circle.stroke(0xff0000);
+
+    // Add the circle to the stage
+    app.stage.addChild(circle);
+
+}
+
+export function movePlayer(app: Application, direction: string) {
+    if (direction === "up") {
+        player.y += 20
+    } else if (direction === "down") {
+        player.y -= 20
+
+    } else if (direction === "left") {
+        player.x -= 20
+
+    } else if (direction === "right") {
+        player.x += 20
+
+    }
 
 
 
 }
+
+
+
+
+
+
