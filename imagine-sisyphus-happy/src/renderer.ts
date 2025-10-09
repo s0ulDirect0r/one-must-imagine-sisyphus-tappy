5b64ffc087695e3966a942e8e4c4b08735344fef
rpasetes <russap01@gmail.co>
2025-10-09 17:38:34 -0400

    animation todo

--- imagine-sisyphus-happy/src/renderer.ts
+++ imagine-sisyphus-happy/src/renderer.ts
@@ -52,10 +52,12 @@ export async function initialize(gameState: GameState) {
 
   const animationDuration = 250;
   app.stage.addChild(border);
-  // TODO: change ticker timing to sync with bg
+  // TODO: implement border fade with background logic??
   app.ticker.add((ticker) => {
-    const progress = ticker.deltaMS / animationDuration
-    border.alpha = Math.min(1, progress)
+    if (gameState.expectMove) {
+      border.alpha = 1;
+    }
+    border.alpha -= ticker.deltaMS;
   });
 
   // const myGrid = initializeGrid();
